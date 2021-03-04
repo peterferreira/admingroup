e8ca081be8cac6db45503e984159b6a746a12458
Peter Ferreira <peterkid@gmail.com>
2021-03-04 12:06:19 -0300

    Correções no requirements e remoção de modulos não necessários

--- core/models.py
+++ core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.urls import reverse
-from phonenumbers import parse, geocoder
 import requests
 
 # Create your models here.
